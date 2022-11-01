7764fe9394654f857bca8909cfc8ea7e0f496ae0
IanDelMar <42134098+IanDelMar@users.noreply.github.com>
2022-11-01 21:27:50 +0100

    replace aria-labelledby with aria-label

--- global-templates/navbar-collapse-bootstrap5.php
+++ global-templates/navbar-collapse-bootstrap5.php
@@ -11,12 +11,7 @@ defined( 'ABSPATH' ) || exit;
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
-
-	<h2 id="main-nav-label" class="screen-reader-text">
-		<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
-	</h2>
-
+<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-label="<?php esc_html_e( 'Menu', 'understrap' ); ?>">
 
 	<div class="<?php echo esc_attr( $container ); ?>">
 
